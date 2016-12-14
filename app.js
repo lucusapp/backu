fec108d3f27ce0ed09c3929ad7f4e9dce89f9b5f
lucusapp <lucusapp@gmail.com>
2016-12-14 16:36:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
 var express = require('express'),
+    mongoskin = require('mongoskin'),
     config  = require('./config/config');
 
 var app     = express();
+
+var db = mongoskin.db('mongodb://@localhost:27017/testdatabase', {safe:true});
+var id = mongoskin.helper.toObjectID;
+
+var allowMethods = function (req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+}
+
+//http://localhost:3000/api/:coleccion/.id
+app.param('coleccion' function(req, res, next, coleccion) {
+    req.collection = db.collection(coleccion);
+});
+
+//PARA COGER LOS DATOS DE REQ.BODY TENEMOS QUE DECIRLE A NUESTRO MIDDLEWARE QUE USE BODYPARSER
+
+app.use(bodyParser.urlenconde({extended: true}));
+app.use(bodyParser.json());
+app.user(allowMethods);
+
+
+//post
+app.post('/api/:coleccion', function(req, res, next) {
+    req.collection.insert(req.body, {}, function(e, result) {
+        if(e) return next(e);
+          res.send(result);
+    });
+});
+
+app.get('/api/:coleccion', function(req, res, next) {
+    req.collection.find({}, {limit: 10, sort[['_id', -1]]}).toArray(function(e, results) {
+        if(e) return next(e);
+        res.send(results);
+    });  
+});
+
+app.get('/api/:coleccion/:id', function(req, res, next) {
+    req.collection.findOne({_id:id(req.params.id)}, function(e, result) {
+        if(e) return next(e);
+           res.send(result);
+    });
+})
+
+
+                                                                  
 
 require('./config/express')(app, config);
 require('./app/routes')(app);
