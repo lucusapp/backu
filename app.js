a92df8517eb3cd39279200f3a6574abf659b3299
lucusapp <lucusapp@gmail.com>
2016-12-14 19:55:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ var id = mongoskin.helper.toObjectID;
 
 var allowMethods = function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+};
+
+var allowCrossTokenHeader = function(req, res, next) {
+    res.headers('Access-Control-Allow-Headers', 'token');
+};
+
+var auth = function(req, res, next) {
+    if(req.headers.token === "password1234") {
+        return next();
+    } else {
+        return next (new Error('no autorizado'));
+    }
 }
+      
 
 //http://localhost:3000/api/:coleccion/.id
 app.param('coleccion' function(req, res, next, coleccion) {
@@ -20,31 +33,49 @@ app.param('coleccion' function(req, res, next, coleccion) {
 
 app.use(bodyParser.urlenconde({extended: true}));
 app.use(bodyParser.json());
-app.user(allowMethods);
+app.use(allowMethods);
+app.use(allowCrossTokenHeader);
 
 
 //post
-app.post('/api/:coleccion', function(req, res, next) {
+app.post('/api/:coleccion', auth,  function(req, res, next) {
     req.collection.insert(req.body, {}, function(e, result) {
         if(e) return next(e);
           res.send(result);
     });
 });
 
-app.get('/api/:coleccion', function(req, res, next) {
+app.get('/api/:coleccion', auth, function(req, res, next) {
     req.collection.find({}, {limit: 10, sort[['_id', -1]]}).toArray(function(e, results) {
         if(e) return next(e);
         res.send(results);
     });  
 });
 
-app.get('/api/:coleccion/:id', function(req, res, next) {
+app.get('/api/:coleccion/:id', auth, function(req, res, next) {
     req.collection.findOne({_id:id(req.params.id)}, function(e, result) {
         if(e) return next(e);
            res.send(result);
     });
 })
 
+//PUT
+app.put('/api/:coleccion/:id', auth, function(req, res, next) {
+    req.collection.update(´_id: id(req.params.id)}, {$set: req.body}, {safe: true, multi: false},
+       function(e, result) {
+            if(e) return next(e);
+    res.send((result === 1) ? {resultado: "ok"} : {resultado: "ko"});
+    });
+});
+
+//DELETE
+
+app.delete('/api/:coleccion/.id, auth, function() {
+    req.collection.remove({_id: id(req.params.id), function(e, result) {
+        if(e) return next (e);
+         res.send(result ===1} ? {resultado: "ok"} : {resultado: "ko"});
+    });
+});
 
                                                                   
 
